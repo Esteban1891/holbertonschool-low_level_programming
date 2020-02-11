67f128a5c73f2b3312f6f4e7122253c592e06037
Esteban1891 <esteban.delahoz15@gmail.com>
2020-02-11 18:48:44 -0500

    0x03. C - Debugging

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,11 +1,14 @@
-# ifndef HOLBERTON_H
-# define  HOLBERTON_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-# include  < stdio.h >
+#include <stdio.h>
 
-nulo  positivo_o_negativo ( int i);
-int  mayor_número ( int , int , int );
-int  convert_day ( int mes, int día);
-void  print_remaining_days ( int mes, int día, int año);
+void positive_or_negative(int i);
 
-# endif  / * HOLBERTON_H * /
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
+
+#endif /* HOLBERTON_H */

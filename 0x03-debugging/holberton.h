036fbf2ec28eec3047a7868082a5279da4c0f33a
Esteban1891 <esteban.delahoz15@gmail.com>
2020-02-11 17:34:48 -0500

    fix error

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,11 @@
+# ifndef HOLBERTON_H
+# define  HOLBERTON_H
+
+# include  < stdio.h >
+
+nulo  positivo_o_negativo ( int i);
+int  mayor_número ( int , int , int );
+int  convert_day ( int mes, int día);
+void  print_remaining_days ( int mes, int día, int año);
+
+# endif  / * HOLBERTON_H * /

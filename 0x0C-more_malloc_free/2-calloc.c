6019ef0dee9db041b8438b09ce3e444512817d01
Esteban1891 <60367971+Esteban1891@users.noreply.github.com>
2020-03-05 15:47:14 -0500

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,18 +11,17 @@
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
     char *str;
     unsigned int x;
+    
     
 if (nmemb == 0 || size == 0)
 return (NULL);
 str = malloc(nmemb * size);
 if (str == NULL)
 return (NULL);
-for (x = 0 ; x < (size * nmemb) ; x++);
-*(str + (x)) = 0;
+for (x = 0; x < (size * nmemb); x++)
+str[x] = 0;
 return(str);
-
 }

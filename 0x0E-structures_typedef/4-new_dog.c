5da24735275d30fdea6e583cec12f9ca545feaf8
Esteban1891 <60367971+Esteban1891@users.noreply.github.com>
2020-03-09 20:04:46 -0500

    Update 4-new_dog.c
    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,35 +1,57 @@
-#include "dog.h"
+ #include "dog.h"
+/**
+ * *_strcpy - copy string
+ * @dest: char to test
+ * @src: char to test
+ *
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
 
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+
+dest[i] = '\0';
+return (dest);
+}
 
 /**
- * new_dog - entry point
- * @name: string from main, name of pet
- * @age: number from main, age of pet
- * @owner: string from main, owner of pet
- * Return: p
+ * new_dog - Creates a new structure dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: Return a pointer to the structure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
 		return (NULL);
-	/* Cpunting name pointer*/
+
+	(*ptr).name = malloc(sizeof(name) + 1);
 	if (name == NULL)
 	{
-		free(p);
-		free(owner);
+		free(ptr);
 		return (NULL);
 	}
-	if (owner == NULL)
+
+	(*ptr).owner = malloc(sizeof(owner) + 1);
+	if ((*ptr).owner == NULL)
 	{
-		free(p);
-		free(name);
+		free((*ptr).name);
+		free(ptr);
 		return (NULL);
 	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+
+	_strcpy((*ptr).name, name);
+	_strcpy((*ptr).owner, owner);
+	(*ptr).age = age;
+
+	return (ptr);
 }

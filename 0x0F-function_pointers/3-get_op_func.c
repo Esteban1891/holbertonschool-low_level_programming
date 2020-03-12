63da26a5bae53d4d00faf8515fd5bfcc5986481b
Esteban1891 <60367971+Esteban1891@users.noreply.github.com>
2020-03-11 22:31:26 -0500

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,12 @@
 #include "3-calc.h"
-#include <stddef.h>
+#include <string.h>
 
 /**
- * get_op_func - Test
- * @s: String to compare
- * Return: Pointer to function
- **/
+ * get_op_func - selects the correct operation toperform
+ * @s: operation to perform
+ *
+ * Return: pointer to the correct function
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -21,12 +22,9 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (!strcmp(ops[i].op, s))
-			return (ops[i].f);
-
+		if (strcmp(s, ops[i].op) == 0)
+			break;
 		i++;
 	}
-
-
-	return (NULL);
+	return (ops[i].f);
 }

18fbe699f2c302c68165e5555eba2dafceb8fe74
Esteban1891 <60367971+Esteban1891@users.noreply.github.com>
2020-03-11 22:36:00 -0500

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,45 +1,33 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
+
 /**
- * main - entry point
- * @argc: arguments number
- * @argv: double pointer to arguments
- * Return: O for success
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
+ *
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	/* Variables for the two entry numbers */
-	int num1 = 0, num2 = 0, calc = 0;
-	/* If arguments is wrong */
-	if (argc < 4)
+	int a, b, c;
+	int (*f)(int, int);
+
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	/* convert arguments to numbers */
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-	switch (argv[2][0])
-	{
-		case '+':
-		case '-':
-		case '*':
-		case '/':
-		case '%':
-			calc = (*get_op_func(argv[2]))(num1, num2);
-			printf("%d\n", calc);
-		break;
-		default:
-			printf("Error\n");
-			exit(99);
-	}
-	/* Print operation */
+	c = f(a, b);
+	printf("%d\n", c);
 	return (0);
 }

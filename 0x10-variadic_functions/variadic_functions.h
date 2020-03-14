1278ccfd7e09151343744e8d783ec6f7051f4e0b
root <Esteban1891>
2020-03-14 15:05:48 -0500

    array

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,10 @@
-#ifndef VARIADIC_FUNCTIONS_H_
-#define VARIADIC_FUNCTIONS_H_
+#ifndef __VARIADIC_H__
+#define __VARIADIC_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdarg.h>
-
+#include <stdio.h>
 int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -17,11 +14,16 @@ void print_int(va_list);
 void print_float(va_list);
 void print_string(va_list);
 
-
+/**
+ * struct format_t - entry signs
+ *
+ * @t: type of variables
+ * @f: function to print type of variable
+*/
 typedef struct format_t
 {
 	char *t;
 	void (*f)(va_list);
 } format_t;
 
-#endif
+#endif /* VARIADIC_FUNCTIONS.H */

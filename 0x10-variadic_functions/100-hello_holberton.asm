d4a346c133c4296a2c679a13659c370a57b6301d
root <Esteban1891>
2020-03-14 20:15:44 -0500

    advance in assambler

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,12 +1,16 @@
-; Define variables in the data section
+				; Define variables in the data section
+	; estas son variables
 SECTION .DATA
-	tupapiesteban:     db 'Hello Holberton!',10
-	ellargodetupapi:  equ $-hello
+	tupapiesteban:     db 'Hello Holberton!',10 ; char que almacena mi strings
+        ellargodetupapi:  equ $-hello ; este seria el largo de mi string
 
 ; Code goes in the text section
 SECTION .TEXT
 	global main
-
+	; esto que esta abajo es mi main
+  	; la cual hace los procesos requeridos de esto write(int, message, bytes)
+       	; esto es basicamente lo que se encuentra abajo en modelo assambler
+	; ya que llamamos al kernel despues la funcion write despues los 3 parametros como instrucciones separadas
 main:
 	int 80h              ; call the kernel
 	mov eax,4            ; system call write function = 4

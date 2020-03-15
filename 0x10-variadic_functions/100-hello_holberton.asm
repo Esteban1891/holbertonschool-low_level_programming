011ff88269261852d4abd31a7a3e3273611a3494
root <Esteban1891>
2020-03-14 19:41:39 -0500

    advance

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,22 +1,16 @@
-	.file	"hello.c"
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton!"
-	.text
-	.globl	main
-	.type	main, @function
+global main
+
+	section .text
 main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
-	call	puts
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
+
+	mov eax, 60
+	xor rdi, rdi
+	syscall
+
+message:
+	db "Hello, Holberton", 10

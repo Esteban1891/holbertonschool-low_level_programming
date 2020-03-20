cd2487b963f507daec5871e097a302bf0543e343
Esteban1891 <esteban.delahoz15@gmail.com>
2020-03-19 21:22:57 -0500

    errors

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,3 @@
-
 #ifndef LIST_H
 #define LIST_H
 #include <stdio.h>
@@ -17,6 +16,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
@@ -24,4 +24,5 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(const char *s);
 void print_beforemain(void) __attribute__((constructor));
+
 #endif

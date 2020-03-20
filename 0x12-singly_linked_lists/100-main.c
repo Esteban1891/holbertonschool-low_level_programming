5f186766e02a01a235b8b21c80e6dd8a39c0800c
Esteban1891 <esteban.delahoz15@gmail.com>
2020-03-19 22:07:59 -0500

    solution

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int print_esteban(void)
 {
 	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 	return (0);

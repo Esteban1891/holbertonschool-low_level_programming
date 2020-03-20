45f8906ae23f5bde7af2c84482fe13d4b01f2762
Esteban1891 <60367971+Esteban1891@users.noreply.github.com>
2020-03-19 22:12:58 -0500

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,10 +4,10 @@
 void __attribute__((constructor)) hare(void);
 
 /**
- *print_esteban - prints a hare before the main
+ *@hare - prints a hare before the main
  */
 
-void print_esteban(void)
+void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	       "I bore my house upon my back!\n");

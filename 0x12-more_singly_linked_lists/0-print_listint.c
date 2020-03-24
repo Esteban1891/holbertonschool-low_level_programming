ece3b10bb7992de2237885d31f217bf6163a48a4
Esteban1891 <esteban.delahoz15@gmail.com>
2020-03-24 02:41:23 -0500

    problems with alias

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -7,18 +7,19 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int count;
+	size_t count = 0;
 
-	count = 0;
+	if (h == NULL)
+
+		return (0);
+
 	while (h != NULL)
 	{
-
-
-		printf("%d\n", h->n);
+		printf("%u\n", h->n);
 		h = h->next;
 		count++;
-
-
 	}
+
+
 	return (count);
 }

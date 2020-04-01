9a61d0c11f7286efe03eab9fa4857dd147499c79
Esteban1891 <esteban.delahoz15@gmail.com>
2020-03-31 21:41:23 -0500

    create to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * create_file - create a file with the text content
  * @filename: name of the file to open
@@ -8,8 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, count = 0;
-        int number = 1;
+	int fd, number = 1, count = 0;
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)

e05effdd25be943e8e5392d3d69b5f283e6fa894
Esteban1891 <esteban.delahoz15@gmail.com>
2020-03-31 20:05:30 -0500

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,15 +2,15 @@
 
 /**
  * read_textfile - read txt files and prints it in the POSIX output
- * @filename: input name of the file in string
- * @letters: input number of letter it should print
- * Return: Always 0 if failes or the size of the print
+ * @filename: name of the file to open
+ * @letters:  number of letter of my input
+ * Return: number of bytes or letter
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t num;
 	int fd;
-	char *buf;
+	char *num_bytes;
 
 	if (filename == NULL)
 		return (0);
@@ -20,11 +20,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(letters);
 	if (buf == NULL)
 		return (0);
-	num = read(fd, buf, letters);
+	num = read(fd, num_bytes, letters);
 	if (num == -1)
 		return (0);
-	buf[num] = '\0';
-	write(STDOUT_FILENO, buf, num);
+	num_bytes[num] = '\0';
+	write(STDOUT_FILENO, num_bytes, num);
 	close(fd);
 	return (num);
 }

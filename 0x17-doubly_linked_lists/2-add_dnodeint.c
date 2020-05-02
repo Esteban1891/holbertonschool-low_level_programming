80ab5d7910b73bab4918d4132701515b8cfce0d3
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-02 12:13:46 -0500

    array

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	if (head == NULL)
-		
+		return (NULL);
+
 		new = malloc(sizeof(dlistint_t));
 	*head = new;
 	new->n = n;
